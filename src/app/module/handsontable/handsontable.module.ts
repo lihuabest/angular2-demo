4021a61e2e7f65758f2e8df11b8f224bf121c108
lihua <lihuabest@gmail.com>
2017-07-31 15:49:06 +0800

    update

--- src/app/module/handsontable/handsontable.module.ts
+++ src/app/module/handsontable/handsontable.module.ts
@@ -6,6 +6,15 @@ import {HandsontableTableComponent} from "./components/table/handsontable.table.
 /**
  * https://github.com/handsontable/handsontable/issues/4053
  * handsontable  不能直接在angular-cli里配置 目前是直接修改cli的common
+ *
+ * module.exports = {
+      module: {
+            noParse: [path.join(__dirname, "node_modules/handsontable/dist/handsontable.full.js")]
+      },
+      plugins: [new webpack.ProvidePlugin({
+          "Handsontable": "handsontable/dist/handsontable.full.js"
+      })],
+    }
  */
 @NgModule({
     imports: [
