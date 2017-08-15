ed6ab0847898e68f4c1f7016113450176c273fe1
李华 <lihuabest@qq.com>
2017-08-15 20:28:38 +0800

    update grid

--- src/app/module/handsontable/handsontable.module.ts
+++ src/app/module/handsontable/handsontable.module.ts
@@ -7,6 +7,9 @@ import {HandsontableTableComponent} from "./components/table/handsontable.table.
  * https://github.com/handsontable/handsontable/issues/4053
  * handsontable  不能直接在angular-cli里配置 目前是直接修改cli的common
  *
+ * https://stackoverflow.com/questions/44566898/zoneawarepromise-overwritten-when-adding-handsontable-to-angular-cli-scripts
+ * 这篇文章也是一个解决方案
+ *
  * module.exports = {
       module: {
             noParse: [path.join(__dirname, "node_modules/handsontable/dist/handsontable.full.js")]
